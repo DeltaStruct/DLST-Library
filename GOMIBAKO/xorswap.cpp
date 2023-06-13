47712c99952d355d258c18679b4fca553295d075
DeltaStruct <DeltaStruct47@gmail.com>
2023-06-13 11:19:38 +0900

    Update xorswap.cpp

--- GOMIBAKO/xorswap.cpp
+++ GOMIBAKO/xorswap.cpp
@@ -1,7 +1,7 @@
-// GOMIBAKO
 // Copyright (c) 2023 DeltaStruct
 // Released under the MIT license
 // https://github.com/DeltaStruct/DLTS-Library/blob/main/LICENSE
+// カテゴリ:GOMIBAKO
 // 廃棄理由:テスト用(同じ値のときに正しく動作しない ゴミ箱テスト用なので条件分岐入れない)
 
 template<typename T>
