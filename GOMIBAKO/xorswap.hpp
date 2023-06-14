c03f57d0cee79df151eef6b5dd7e82933a67e7f6
DeltaStruct <DeltaStruct47@gmail.com>
2023-06-14 09:23:32 +0900

    Update xorswap.hpp

--- GOMIBAKO/xorswap.hpp
+++ GOMIBAKO/xorswap.hpp
@@ -3,9 +3,10 @@
 // https://github.com/DeltaStruct/DLTS-Library/blob/main/LICENSE
 // カテゴリ:GOMIBAKO
 // 廃棄理由:テスト用(同じ値のときに正しく動作しない ゴミ箱テスト用なので条件分岐入れない)
+namespace dlts {
+  template<typename T>
+  void xorswap(T& x, T& y) noexcept { x^=y,y^=x,x^=y; }
 
-template<typename T>
-void xorswap(T& x, T& y) noexcept { x^=y,y^=x,x^=y; }
-
-template<typename T>
-void xorswap(T* x, T* y) noexcept { *x^=*y,*y^=*x,*x^=*y; }
+  template<typename T>
+  void xorswap(T* x, T* y) noexcept { *x^=*y,*y^=*x,*x^=*y; }
+}
